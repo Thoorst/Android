bc2655b18b2b30f4b55097b8da6e1ef26bd033ba
Thoorst <32663874+Thoorst@users.noreply.github.com>
2017-10-22 20:46:14 +0200

    002 Skońcona baza

--- app/src/main/java/com/example/mateusz/mathewandroid/DbHelper.java
+++ app/src/main/java/com/example/mateusz/mathewandroid/DbHelper.java
@@ -5,19 +5,36 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
+import java.security.PrivateKey;
+
 /**
  * Created by Mateusz on 2017-10-22.
  */
 
-public final class DbHelper {
-    private DbHelper() {}
+public final class DbHelper extends SQLiteOpenHelper{
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "DbHelper.db";
+
+    private static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" + FeedEntry._ID + "INTEGER PRIMARY KEY," + FeedEntry.COLUMN_NAME_TITLE + " TEXT," + FeedEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
+    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS" + FeedEntry.TABLE_NAME;
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
     public static class FeedEntry implements BaseColumns {
         public static final String TABLE_NAME = "entry";
         public static final String COLUMN_NAME_TITLE = "title";
         public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    }
 
-        private static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" + FeedEntry._ID + "INTEGER PRIMARY KEY," + FeedEntry.COLUMN_NAME_TITLE + " TEXT," + FeedEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
-        private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS" + FeedEntry.TABLE_NAME;
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_ENTRIES);
     }
 }
